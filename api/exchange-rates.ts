2a77bcbf31594e6823f2de84c6b6ef307f5dbf9c
Geoff <git@geoffdick.com>
2021-03-08 08:51:03 -0700

    ⚡ feat: Change api base to USD

--- api/exchange-rates.ts
+++ api/exchange-rates.ts
@@ -5,7 +5,7 @@ export default function (req: NowRequest, res: NowResponse) {
   // Cache results for 24h
   res.setHeader('Cache-Control', 's-maxage=86400')
 
-  axios.get('https://api.exchangeratesapi.io/latest')
+  axios.get('https://api.exchangeratesapi.io/latest?base=USD')
   .then(function (response) {
     res.status(200).send(response)
   })
