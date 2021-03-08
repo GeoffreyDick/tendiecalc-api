2f65509664340bfa3b3522dd234031f8e90c7e9f
Geoff <git@geoffdick.com>
2021-03-08 11:32:44 -0700

    🐛 fix: Move headers to vercel.json

--- api/exchange-rates.ts
+++ api/exchange-rates.ts
@@ -1,25 +1,7 @@
 import { NowRequest, NowResponse } from '@vercel/node'
 const axios = require('axios');
 
-const allowCors = fn => async (req: NowRequest, res: NowResponse) => {
-  // Cache results for 6h
-  res.setHeader('Cache-Control', 's-maxage=21600')
-
-  // Allow CORS
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
-const handler = async (req: NowRequest, res: NowResponse) => {
+export default async function (req: NowRequest, res: NowResponse) {
   try {
     const response = await axios.get('https://api.exchangeratesapi.io/latest?base=USD').then(({data}) => data)
     res.send(response)
@@ -27,5 +9,3 @@ const handler = async (req: NowRequest, res: NowResponse) => {
     res.send(error)
   }
 }
-
-module.exports = allowCors(handler)
