755db3863a18b1fb92b1bbefef464ffdb0b7a496
Geoff <git@geoffdick.com>
2021-03-08 09:42:50 -0700

    🐛 fix: Add destructured data

--- api/exchange-rates.ts
+++ api/exchange-rates.ts
@@ -1,12 +1,12 @@
 import { NowRequest, NowResponse } from '@vercel/node'
 const axios = require('axios');
 
-module.exports = async function (req: NowRequest, res: NowResponse) {
+export default async function (req: NowRequest, res: NowResponse) {
   // Cache results for 24h
   res.setHeader('Cache-Control', 's-maxage=0')
 
   try {
-    const response = await axios.get('https://api.exchangeratesapi.io/latest?base=USD')
+    const response = await axios.get('https://api.exchangeratesapi.io/latest?base=USD').then(({data}) => data)
     res.send(response)
   } catch (error) {
     res.send(error)
