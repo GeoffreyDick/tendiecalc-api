b2c51d98c9fb239608b287f6412f1874ffc29e7a
Geoff <git@geoffdick.com>
2021-03-08 11:10:33 -0700

    ⚡ feat: Add CORS header to exchange-rates res

--- api/exchange-rates.ts
+++ api/exchange-rates.ts
@@ -4,6 +4,8 @@ const axios = require('axios');
 export default async function (req: NowRequest, res: NowResponse) {
   // Cache results for 6h
   res.setHeader('Cache-Control', 's-maxage=21600')
+  // Allow CORS
+  res.setHeader('Access-Control-Allow-Origin', '*')
 
   try {
     const response = await axios.get('https://api.exchangeratesapi.io/latest?base=USD').then(({data}) => data)
