767e2ec7f1ecdaf31057081d98fec82ca1dd3b5a
Geoff <git@geoffdick.com>
2021-03-08 09:38:53 -0700

    🐛 fix: Add Vercel build script?

--- api/exchange-rates.ts
+++ api/exchange-rates.ts
@@ -6,7 +6,7 @@ export default async function (req: NowRequest, res: NowResponse) {
   res.setHeader('Cache-Control', 's-maxage=0')
 
   try {
-    const response = await axios.get('https://api.exchangeratesapi.io/latest?base=USD').then((data) => data)
+    const response = await axios.get('https://api.exchangeratesapi.io/latest?base=USD')
     res.send(response)
   } catch (error) {
     res.send(error)
