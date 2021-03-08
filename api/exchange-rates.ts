0fba8874afd8d1c5efa94b0d29c9346a340cd78f
Geoff <git@geoffdick.com>
2021-03-08 08:48:07 -0700

    ⚡ feat: Add axios

--- api/exchange-rates.ts
+++ api/exchange-rates.ts
@@ -1,6 +1,15 @@
 import { NowRequest, NowResponse } from '@vercel/node'
+import axios from 'axios'
 
 export default function (req: NowRequest, res: NowResponse) {
-  const { name = 'World' } = req.query
-  res.send(`Hello ${name}!`)
+  // Cache results for 24h
+  res.setHeader('Cache-Control', 's-maxage=86400')
+
+  axios.get('https://api.exchangeratesapi.io/latest')
+  .then(function (response) {
+    res.status(200).send(response)
+  })
+  .catch(function (error) {
+    res.send(error);
+  })
 }
