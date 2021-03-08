312960b0f5497fb71e7c852159bb635a6621c7ec
Geoff <git@geoffdick.com>
2021-03-08 09:45:31 -0700

    ⚡ feat: Add caching to exchange-rates

--- api/exchange-rates.ts
+++ api/exchange-rates.ts
@@ -2,8 +2,8 @@ import { NowRequest, NowResponse } from '@vercel/node'
 const axios = require('axios');
 
 export default async function (req: NowRequest, res: NowResponse) {
-  // Cache results for 24h
-  res.setHeader('Cache-Control', 's-maxage=0')
+  // Cache results for 6h
+  res.setHeader('Cache-Control', 's-maxage=21600')
 
   try {
     const response = await axios.get('https://api.exchangeratesapi.io/latest?base=USD').then(({data}) => data)
