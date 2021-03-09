bdc0fa0556d23232613083f4304c66c937c9a8bd
Geoff <git@geoffdick.com>
2021-03-08 22:26:18 -0700

    🐛 fix: Fix 404 error Pt. 2

--- api/marketstack.ts
+++ api/marketstack.ts
@@ -3,14 +3,13 @@ const axios = require('axios');
 const ticker = 'GME'
 const params = {
   access_key: process.env.MARKETSTACK_API_KEY,
-  exchange: 'NYSE',
   interval: '15min'
 
 }
 
 export default async function (req: NowRequest, res: NowResponse) {
   try {
-    const response = await axios.get(`https://api.marketstack.com/v1/tickers/GME/intraday/latest`, { params }).then(({data}) => data)
+    const response = await axios.get(`https://api.marketstack.com/v1/tickers/${ticker}/intraday/latest`, { params }).then(({data}) => data)
     res.send(response)
   } catch (error) {
     res.send(error)
